855da4283290bd973821dc83d28c8b740cfea6a8
zohraBarkouch <fbarkouch@orsys.co.ma>
2024-05-20 11:50:18 +0100

    Add SonarQube analysis configuration

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,6 @@
 import com.example.Calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.Test;
 
  class CalculatorTest {
